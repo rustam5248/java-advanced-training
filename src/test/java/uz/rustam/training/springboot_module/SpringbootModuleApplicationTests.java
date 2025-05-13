b3720349221b736428f10b61c4dfd2893c8af790
Rustam Yusupov <rustam_yusupov@epam.com>
2025-05-13 20:37:01 +0500

    The first commit.

---
+++ src/test/java/uz/rustam/training/springboot_module/SpringbootModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.rustam.training.springboot_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
