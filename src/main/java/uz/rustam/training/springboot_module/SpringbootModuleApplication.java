b11b70aa6c590b2cea790d28b8f5b57e404a2c79
Rustam Yusupov <rustam.r.yu@gmail.com>
2025-05-13 21:00:40 +0500

    Add hello-world application, task1.

--- src/main/java/uz/rustam/training/springboot_module/SpringbootModuleApplication.java
+++ src/main/java/uz/rustam/training/springboot_module/SpringbootModuleApplication.java
@@ -1,13 +1,19 @@
 package uz.rustam.training.springboot_module;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootModuleApplication {
+public class SpringbootModuleApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootModuleApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args){
+		System.out.println("hello world");
 	}
 
 }
